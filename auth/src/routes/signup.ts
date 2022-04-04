e0ab958d314cc900f523a19d032d9420b7dabdb3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-04-04 21:18:23 +0100

    feat: add validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there!');
+const validation = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters'),
+];
+
+router.post('/api/users/signup', validation, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
+  const { email, password } = req.body;
+
+  console.log('Creating user with email: ' + email);
+
+  res.send({});
 });
 
 export { router as signupRouter };
