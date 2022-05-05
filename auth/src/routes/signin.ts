10753ba18a345f8ab2bce7622000abea3bd99725
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-05-05 06:17:19 +0100

    feat: add request validation on signin router

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,19 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors';
 
 const router = Router();
 
-router.post('/api/users/signin', (req, res) => {
+const validation = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('You must supply a password'),
+];
+
+router.post('/api/users/signin', validation, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
   res.send('Hi there!');
 });
 
