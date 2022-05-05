9d88e5c09a9365f095144f0913ded6ffdb2cda56
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-05-05 04:49:14 +0100

    feat: remove _id and __v

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -30,6 +30,14 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+      },
+    },
   }
 );
 
