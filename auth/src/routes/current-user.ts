06cf73a69f8c96be09dd81221bf7baa90efd3208
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-05-05 07:08:44 +0100

    feat: add current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
