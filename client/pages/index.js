884727c6576c56a4153fea941f1d3fe623c51ae6
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-07-24 23:47:45 +0100

    feat: load current user on ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,19 @@
 import { buildClient } from '../api';
 
 const LandingPage = ({ currentUser }) => {
-  if (currentUser) {
-    console.log(currentUser);
-    return (
-      <>
-        <br />
-        <h1>YOU ARE SIGN IN</h1>
-      </>
-    );
-  }
-  return <h1>Landing Page1</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (constext) => {
   const client = buildClient(constext);
-  const { data } = await client.get('/api/users/me');
+  const { data } = await client.get('/api/users/me').catch((error) => {
+    console.log(error);
+    return { currentUser: null };
+  });
   return data;
 };
 
