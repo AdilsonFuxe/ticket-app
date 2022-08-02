8f7e36f5c20c41bb8c6e86222978facd71221e32
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-02 01:58:20 +0100

    fix: fix mongo url

--- tickets/src/main/config/env.ts
+++ tickets/src/main/config/env.ts
@@ -1,7 +1,7 @@
 import './setup-env';
 
 export default {
-  mongoUrl: 'mongodb://ticket-mongo-srv:27017/ticket',
+  mongoUrl: 'mongodb://tickets-mongo-srv:27017/tickets',
   port: 3000,
   jwtSecret: process.env.JWT_KEY!,
   salt: 12,
