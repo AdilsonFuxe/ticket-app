dda64c69a0a23c51c5e80c887283332d7006fb8a
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-02 02:48:29 +0100

    feat: add mongo uri in auth service

--- auth/src/main/config/env.ts
+++ auth/src/main/config/env.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 export default {
-  mongoUrl: 'mongodb://auth-mongo-srv:27017/auth',
+  mongoUrl: process.env.MONGO_URI!,
   port: 3000,
   jwtSecret: process.env.JWT_KEY!,
   salt: 12,
--- auth/src/main/server.ts
+++ auth/src/main/server.ts
@@ -7,12 +7,15 @@ const start = async () => {
   if (!env.jwtSecret) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!env.mongoUrl) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
     await mongoose.connect(env.mongoUrl);
   } catch (error) {
     console.error(error);
   }
-  app.listen(3000, () => {
+  app.listen(env.port, () => {
     console.log('Server is running on port 3000!!');
   });
 };
