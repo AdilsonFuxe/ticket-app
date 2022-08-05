c546ad53d372b8ef85e655036715c103ee778357
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-05 03:40:04 +0100

    chore: add collect coverage from

--- tickets/jest.config.ts
+++ tickets/jest.config.ts
@@ -1,7 +1,13 @@
 export default {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/**/protocols.ts',
+    '!**/protocols/**',
+  ],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
