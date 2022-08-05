48e25fb3f64df36165e901ab6d1986d2ca45d772
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-05 01:36:18 +0100

    test: ensure loadTicketsRepository return a list of two tickets on success

---
+++ tickets/src/infra/db/mongoose/repositories/__tests__/load-tickets-repository.spec.ts
@@ -0,0 +1,39 @@
+import { MongoHelper } from '@src/infra/db/mongoose/helper';
+import { TicketModel } from '@src/infra/db/mongoose/models';
+import {
+  addTicketRepository,
+  loadTicketsRepository,
+} from '@src/infra/db/mongoose/repositories';
+
+const mockAddTicketParams1 = () => ({
+  price: '135.00',
+  title: 'amazon-ticket',
+});
+
+const mockAddTicketParams2 = () => ({
+  price: '200.00',
+  title: 'zap-ticket',
+});
+
+describe('LoadTicketsRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await TicketModel.deleteMany({});
+  });
+
+  it('Should return a list of two tickets on success', async () => {
+    await Promise.all([
+      addTicketRepository(mockAddTicketParams1()),
+      addTicketRepository(mockAddTicketParams2()),
+    ]);
+    const result = await loadTicketsRepository({});
+    expect(result.length).toBe(2);
+  });
+});
