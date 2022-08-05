d4cf1e687388849c51b4d8e9b07beabb6ce8946c
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-05 03:18:53 +0100

    test: ensure requiredFieldValidation return MissingParamError if any parameter is missing

--- tickets/src/validation/validators/required-field-validation.spec.ts
+++ tickets/src/validation/validators/required-field-validation.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '@src/interface/errors';
 import { requiredFieldValidation } from './required-field-validation';
 
 const params = {
@@ -9,5 +10,14 @@ describe('RequiredFieldValidation', () => {
   it('should return null if all parameters are sent', () => {
     expect(requiredFieldValidation('title')(params)).toBeNull();
     expect(requiredFieldValidation('price')(params)).toBeNull();
+  });
+
+  it('should return MissingParamError if any parameter is missing', () => {
+    expect(requiredFieldValidation('title')({ price: 'any_price' })).toEqual(
+      new MissingParamError('title')
+    );
+    expect(requiredFieldValidation('price')({ title: 'any_title' })).toEqual(
+      new MissingParamError('price')
+    );
   });
 });
